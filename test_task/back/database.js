11953a5c9de3fc113271cea5ee077fea87b59cfc
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2023-05-19 00:07:54 +0300

    .

--- test_task/back/database.js
+++ test_task/back/database.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const mysql = require('mysql2')
-const connection = mysql.createConnection('mysql://p672w9wi6g13wsylryan:pscale_pw_bhKQrhVddX9rDMwopdHvFrVWOnMAfovHHqkiI21gV5g@aws.connect.psdb.cloud/besthackathon4yk?ssl={"rejectUnauthorized":true}')
+const connection = mysql.createConnection('mysql://ltanxqcxpvhxywv9i3mk:pscale_pw_mJkdhcBe0dEUVgIyO1phcugcEaRReLRzMite4xOnoP4@aws.connect.psdb.cloud/besthackathon4yk?ssl={"rejectUnauthorized":true}')
 // const connection = mysql.createConnection({
 //     host: 'fdb1029.awardspace.net',
 //     port: 3306,
