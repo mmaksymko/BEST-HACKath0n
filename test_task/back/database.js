b0adf07ffa380947386d45ad28e51c82abc63867
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2023-05-20 05:04:15 +0300

    minor changes and tweaks because of last-minute server change

--- test_task/back/database.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const mysql = require('mysql2')
-const connection = mysql.createConnection(process.env.DATABASE_URL)
-
-setInterval(() => {
-    connection.query('SELECT 1');
-}, 60000);
-
-exports.connection = connection
