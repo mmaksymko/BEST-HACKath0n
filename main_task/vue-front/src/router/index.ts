de13f47e25c70faf23179c0be5218741e158179b
TetianaSendetska <100037892+TetianaSendetska@users.noreply.github.com>
2023-05-27 18:48:28 +0200

    fix router

--- main_task/vue-front/src/router/index.ts
+++ main_task/vue-front/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ChatView from '@/views/ChatView.vue'
+import HelpRequestModalVue from '@/views/RequestHelpView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       name: 'chat',
       component: ChatView
       
+    },
+    {
+      path: '/help',
+      name: 'help',
+      component: HelpRequestModalVue
     }
   ]
 })
