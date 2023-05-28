f48069e5b73d70c49a992ed96a4c78d1a646392e
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2023-05-28 12:09:20 +0300

    [fix] chat move to main server

--- main_task/back/index.js
+++ main_task/back/index.js
@@ -21,19 +21,52 @@ const mongoose = require('mongoose')
 mongoose.connect("mongodb+srv://UserDB:UserDBPassword@maksymcluster.ozlrsuf.mongodb.net/?retryWrites=true&w=majority").then(console.log("Successful"))
     .catch(console.log)
 
+const io = require("socket.io")(http, {
+    cors: {
+        origin: "http://localhost:5173",
+    },
+});
 
+io.use((socket, next) => {
+    const username = socket.handshake.auth.username;
+    if (!username) {
+        return next(new Error("invalid username"));
+    }
+    socket.username = username;
+    next();
+});
 
-// mongoose.connect('mongodb+srv://maksymkomyna:<Qq22012004>@maksymcluster.ozlrsuf.mongodb.net/', { useNewUrlParser: true, useUnifiedtopology: true })
-//     .then(() => console.log("Connected"))
-//     .catch(err => console.log(err))
+io.on("connection", (socket) => {
+    // fetch existing users
+    const users = [];
+    for (let [id, socket] of io.of("/").sockets) {
+        users.push({
+            userID: id,
+            username: socket.username,
+        });
+    }
+    socket.emit("users", users);
 
+    // notify existing users
+    socket.broadcast.emit("user connected", {
+        userID: socket.id,
+        username: socket.username,
+    });
 
-//.then(console.log(x)).catch(e => console.log(e))
+    // forward the private message to the right recipient
+    socket.on("private message", ({ content, to }) => {
+        socket.to(to).emit("private message", {
+            content,
+            from: socket.id,
+        });
+        console.log(content, to, socket.id);
+    });
 
-
-// User.find({}).then(data)
-
-// Chat.find({ "members.user2": 'maksym.myna.pz.2023' }).then(data => data.forEach(dat => console.log(dat.members)))
+    // notify users upon disconnection
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("user disconnected", socket.id);
+    });
+});
 
 
 http.listen(port, () => { })
