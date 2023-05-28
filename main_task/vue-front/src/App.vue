827b688348b2a490a7e69bd9a433df70e8c69355
weroumi <ookk1405@gmail.com>
2023-05-28 05:28:06 +0300

    minor changes

--- main_task/vue-front/src/App.vue
+++ main_task/vue-front/src/App.vue
@@ -74,7 +74,7 @@ async function loginUser(emailParam: string, passwordParam: string){
       "password": passwordParam
     })
   })
-  console.log(response)
+  console.log(response.body)
 }
 
 </script>
